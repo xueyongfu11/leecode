c329d6183e52ba623aaa63f5eec6a38379ed1294
xueyongfu <xueyongfu@datagrand.com>
2023-11-01 19:46:13 +0800

    update

--- NiukeCoding/top101/dynamic-programing/BM73.py
+++ NiukeCoding/top101/dynamic-programing/BM73.py
@@ -9,3 +9,20 @@
 class Solution:
     def getLongestPalindrome(self , A ):
         # write code here
+        dp = [[False] * len(A) for _ in range(len(A))]
+        max_len = 0
+
+        for l in range(0, len(A)):
+            for i in range(0, len(A) - l):
+                j = i + l
+                if l == 0:
+                    dp[i][j] = True
+                elif A[i] == A[j]:
+                    dp[i][j] = dp[i+1][j-1]
+                
+                if dp[i][j]:
+                    max_len = l + 1
+        return max_len
+
+s = Solution()
+print(s.getLongestPalindrome("baabccc"))
